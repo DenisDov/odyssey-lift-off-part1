371f59e75914c19fe8eb4b131c1a725eb06658df
DenisDov <dovzdenis@gmail.com>
2023-10-04 13:30:43 +0300

    install graphql

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { Layout } from "../components";
 
+import { gql } from "../__generated__/";
+
+const TRACKS = gql(`
+  # Query goes here
+  query GetTracks {
+  tracksForHome {
+    id
+    title
+    author {
+      id
+      name
+      photo
+    }
+    thumbnail
+    length
+    modulesCount
+  }
+}
+`);
+
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
